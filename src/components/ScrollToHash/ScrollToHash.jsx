d8c52fb89c7c941fb65aa73879046751247e5b10
numonofcv <numonofcv@gmail.com>
2025-12-17 11:23:01 +0500

    initial

--- src/components/ScrollToHash/ScrollToHash.jsx
+++ src/components/ScrollToHash/ScrollToHash.jsx
@@ -2,20 +2,18 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToHash = () => {
-    const { hash } = useLocation();
+  const { hash } = useLocation();
 
-    useEffect(() => {
-        if (hash && hash !== "#hero") {
-            const el = document.querySelector(hash);
-            if (el) {
-                el.scrollIntoView({ behavior: "smooth" });
-            }
-        } else {
-            window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-        }
-    }, [hash]);
+  useEffect(() => {
+    if (hash && hash !== "#hero") {
+      const el = document.querySelector(hash);
+      if (el) el.scrollIntoView({ behavior: "smooth" });
+    } else {
+      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    }
+  }, [hash]);
 
-    return null;
+  return null;
 };
 
 export default ScrollToHash;
